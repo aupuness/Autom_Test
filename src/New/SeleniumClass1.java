ad22575b1fc3e6eda2580b9d59c5f650bd2ed58e
aupuness <aupuness@gmail.com>
2023-10-17 20:24:22 -0400

    Initial commit

---
+++ src/New/SeleniumClass1.java
@@ -0,0 +1,29 @@
+package New;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		//classname anyname = new classname();
+		
+		//ChromeDriver driver = new ChromeDriver();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		driver.get("www.yahoo.com");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
